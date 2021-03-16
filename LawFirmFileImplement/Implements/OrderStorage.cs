b0782a0d0df5980b9a8c7820d3750b5c3e7e85a4
Сергей Резников <tikto01@mail.ru>
2021-03-16 10:25:42 +0400

    fixed

--- LawFirmFileImplement/Implements/OrderStorage.cs
+++ LawFirmFileImplement/Implements/OrderStorage.cs
@@ -11,125 +11,141 @@ namespace LawFirmFileImplement.Implements
 {
     public class OrderStorage: IOrderStorage
     {
-        private readonly FileDataListSingleton source;
-        public OrderStorage()
-        {
-            source = FileDataListSingleton.GetInstance();
-        }
-        public List<OrderViewModel> GetFullList()
-        {
-            List<OrderViewModel> result = new List<OrderViewModel>();
-            foreach (var order in source.Orders)
-            {
-                result.Add(CreateModel(order));
-            }
-            return result;
-        }
-        public List<OrderViewModel> GetFilteredList(OrderBindingModel model)
-        {
-            if (model == null)
-            {
-                return null;
-            }
-            List<OrderViewModel> result = new List<OrderViewModel>();
-            foreach (var order in source.Orders)
-            {
-                if (order.DocumentId == model.DocumentId)
-                {
-                    result.Add(CreateModel(order));
-                }
-            }
-            return result;
-        }
-        public OrderViewModel GetElement(OrderBindingModel model)
-        {
-            if (model == null)
-            {
-                return null;
-            }
-            foreach (var order in source.Orders)
-            {
-                if (order.Id == model.Id || order.DocumentId ==
-               model.DocumentId)
-                {
-                    return CreateModel(order);
-                }
-            }
-            return null;
-        }
-        public void Insert(OrderBindingModel model)
-        {
-            Order tempOrder = new Order
-            {
-                Id = 1
-            };
-            foreach (var order in source.Orders)
-            {
-                if (order.Id >= tempOrder.Id)
-                {
-                    tempOrder.Id = order.Id + 1;
-                }
-            }
-            source.Orders.Add(CreateModel(model, tempOrder));
-        }
-        public void Update(OrderBindingModel model)
-        {
-            Order tempOrder = null;
-            foreach (var order in source.Orders)
-            {
-                if (order.Id == model.Id)
-                {
-                    tempOrder = order;
-                }
-            }
-            if (tempOrder == null)
-            {
-                throw new Exception("Элемент не найден");
-            }
-            CreateModel(model, tempOrder);
-        }
-        public void Delete(OrderBindingModel model)
-        {
-            Order element = source.Orders.FirstOrDefault(rec => rec.Id == model.Id);
-            if (element != null)
-            {
-                source.Orders.Remove(element);
-            }
-            else
-            {
-                throw new Exception("Элемент не найден");
-            }
-        }
-        private Order CreateModel(OrderBindingModel model, Order order)
-        {
-            order.DocumentId = model.DocumentId;
-            order.DocumentName = model.DocumentName;
-            order.Count = model.Count;
-            order.Status = model.Status;
-            order.DateCreate = model.DateCreate;
-            order.DateImplement = model.DateImplement;
-            return order;
-        }
-        private OrderViewModel CreateModel(Order order)
-        {
-            int money = 0;
-            foreach (var document in source.Documents) {
-                if (document.Id == order.DocumentId) {
-                    money = (int)document.Price * order.Count;
-                    break;
-                }
-            }
-            return new OrderViewModel
-            {
-                Id = order.Id,
-                DocumentId = order.DocumentId,
-                DocumentName = order.DocumentName,
-                Count = order.Count,
-                Sum = money,
-                Status = order.Status,
-                DateCreate = order.DateCreate,
-                DateImplement = order.DateImplement
-            };
-        }
-    }
+		private readonly FileDataListSingleton source;
+
+
+		public OrderStorage()
+		{
+			source = FileDataListSingleton.GetInstance();
+		}
+
+
+		public List<OrderViewModel> GetFullList()
+		{
+			List<OrderViewModel> result = new List<OrderViewModel>();
+			foreach (var order in source.Orders)
+			{
+				result.Add(CreateModel(order));
+			}
+			return result;
+		}
+
+
+		public List<OrderViewModel> GetFilteredList(OrderBindingModel model)
+		{
+			if (model == null)
+			{
+				return null;
+			}
+			List<OrderViewModel> result = new List<OrderViewModel>();
+			foreach (var order in source.Orders)
+			{
+				if (order.DocumentId == model.DocumentId)
+				{
+					result.Add(CreateModel(order));
+				}
+			}
+			return result;
+		}
+
+
+		public OrderViewModel GetElement(OrderBindingModel model)
+		{
+			if (model == null)
+			{
+				return null;
+			}
+			foreach (var order in source.Orders)
+			{
+				if (order.Id == model.Id || order.DocumentId ==
+			   model.DocumentId)
+				{
+					return CreateModel(order);
+				}
+			}
+			return null;
+		}
+
+
+		public void Insert(OrderBindingModel model)
+		{
+			Order tempOrder = new Order
+			{
+				Id = 1
+			};
+			foreach (var order in source.Orders)
+			{
+				if (order.Id >= tempOrder.Id)
+				{
+					tempOrder.Id = order.Id + 1;
+				}
+			}
+			source.Orders.Add(CreateModel(model, tempOrder));
+		}
+
+
+		public void Update(OrderBindingModel model)
+		{
+			Order tempOrder = null;
+			foreach (var order in source.Orders)
+			{
+				if (order.Id == model.Id)
+				{
+					tempOrder = order;
+				}
+			}
+			if (tempOrder == null)
+			{
+				throw new Exception("Элемент не найден");
+			}
+			CreateModel(model, tempOrder);
+		}
+
+
+		public void Delete(OrderBindingModel model)
+		{
+			for (int i = 0; i < source.Orders.Count; ++i)
+			{
+				if (source.Orders[i].Id == model.Id)
+				{
+					source.Orders.RemoveAt(i);
+					return;
+				}
+			}
+			throw new Exception("Элемент не найден");
+		}
+
+
+		private Order CreateModel(OrderBindingModel model, Order order)
+		{
+			order.DocumentId = model.DocumentId;
+			order.DocumentName = model.DocumentName;
+			order.Count = model.Count;
+			order.Status = model.Status;
+			order.DateCreate = model.DateCreate;
+			order.DateImplement = model.DateImplement;
+			return order;
+		}
+
+
+		private OrderViewModel CreateModel(Order order)
+		{
+			decimal money = 0;
+			Document document = source.Documents.FirstOrDefault(rec => rec.Id == order.DocumentId);
+			money = document.Price*order.Count;
+			return new OrderViewModel
+			{
+				Id = order.Id,
+				DocumentId = order.DocumentId,
+				DocumentName = order.DocumentName,
+				Count = order.Count,
+				Sum = money,
+				Status = order.Status,
+				DateCreate = order.DateCreate,
+				DateImplement = order.DateImplement
+			};
+		}
+
+
+	}
 }
